a888044936334ff125f516986c2038f9ffe4ce9b
joshashkinaze <jashkina@umich.edu>
2024-04-03 11:56:50 -0400

    dt to logfil

--- get_tweet_data.py
+++ get_tweet_data.py
@@ -10,8 +10,8 @@ There are two jsonl files that are created.
     When you query Tweepy for a list of tweets and add expansions, then `data` is the primary tweet data and includes_users
     and includes_tweets are the data for the users and tweets that are referenced in the primary tweet data.
 
-- The second is called `{fn}_procd.jsonl` and contains the processed data. The keys are: `original_user_id` and `procd`
-    where `procd` is a list of dicts. The difference between the two is that the `procd` data has been parsed in two key ways.
+- The second is called `{fn}_processed.jsonl` and contains the processed data. The keys are: `original_user_id` and `processed`
+    where `processed` is a list of dicts. The difference between the two is that the `processed` data has been parsed in two key ways.
     First, I collect all the URLs in a nice expanded format (for primary and refd). Second, I also add the author info
     to the data for each ref tweet.
 
@@ -29,6 +29,7 @@ import json
 import tweepy
 import argparse
 import logging
+import datetime
 
 random.seed(416)
 np.random.seed(416)
@@ -40,7 +41,7 @@ client = tweepy.Client(bearer_token=TWITTER_API['bearer_token'], wait_on_rate_li
 
 
 def tweet_controller(user_ids, n, fn):
-    with open(f'{fn}_raw.jsonl', 'w') as raw_file, open(f'{fn}_procd.jsonl', 'w') as procd_file:
+    with open(f'{fn}_raw.jsonl', 'w') as raw_file, open(f'{fn}_processed.jsonl', 'w') as processed_file:
 
         user_id_no_tweets =0
         user_id_errors = 0
@@ -52,29 +53,29 @@ def tweet_controller(user_ids, n, fn):
             try:
                 tweets = get_tweets(user_id, n)
                 if tweets.data:
-                    raw, procd = process_tweets(tweets, user_id)
+                    raw, processed = process_tweets(tweets, user_id)
                     user_id_success += 1
                 else:
                     raw = {'original_user_id': user_id, 'data': -9, 'includes_users': -1, 'includes_tweets': -9}
-                    procd = {'original_user_id': user_id, 'procd': -9}
+                    processed = {'original_user_id': user_id, 'processed': -9}
                     user_id_no_tweets += 1
             except Exception as e:
                 logging.info(f"Error for user {user_id}: ")
                 logging.info(f"Error: {e}")
                 raw = {'original_user_id': user_id, 'data': -1, 'includes_users': -1, 'includes_tweets': -1}
-                procd = {'original_user_id': user_id, 'procd': -1}
+                processed = {'original_user_id': user_id, 'processed': -1}
                 user_id_errors += 1
 
                 # write data
             raw_file.write(json.dumps(raw) + "\n")
-            procd_file.write(json.dumps(procd) + "\n")
+            processed_file.write(json.dumps(processed) + "\n")
 
             counter += 1
             if counter % 50 == 0:
                 logging.info(f"Finished {counter} of {len(user_ids)}")
 
     logging.info("Done")
-    logging.info(f"Errors: {user_id_errors}")
+    logging.info(f"Errors NOT CAUGHT downstream: {user_id_errors}")
     logging.info(f"Success: {user_id_success}")
     logging.info(f"No tweets: {user_id_no_tweets}")
 
@@ -100,6 +101,8 @@ def get_tweets(user_id, n=10):
 
 
 def process_tweets(tweets_response, user_id):
+
+    # Try to get included tweets unless there are no included tweets bundled
     try:
         includes_tweet_data = [tweets_response.includes['tweets'][i].data for i in
                                range(len(tweets_response.includes['tweets']))]
@@ -112,15 +115,22 @@ def process_tweets(tweets_response, user_id):
     except:
         includes_user_data = []
 
+    # Write this raw data to a dict
     basic_data = [tweets_response.data[i].data for i in range(len(tweets_response.data))]
     raw = {'original_user_id': user_id, 'data': basic_data, 'includes_users': includes_user_data,
            'includes_tweets': includes_tweet_data}
 
-    procd = []
+    # Do some processing
+    processed = []
     for i in range(len(tweets_response.data)):
-        procd.append(parse_tweet(tweets_response.data[i].data, includes_tweet_data, includes_user_data))
-    procd = {'original_user_id': user_id, 'procd': procd}
-    return raw, procd
+        try:
+            parsed = parse_tweet(tweets_response.data[i].data, includes_tweet_data, includes_user_data)
+        except:
+            parsed = -1
+        processed.append(parsed)
+
+    processed = {'original_user_id': user_id, 'processed': processed}
+    return raw, processed
 
 
 def parse_tweet(tweet, includes_tweet_data, includes_user_data):
@@ -141,7 +151,6 @@ def parse_tweet(tweet, includes_tweet_data, includes_user_data):
     if 'referenced_tweets' not in tweet:
         tweet['referenced_tweets'] = []
     else:
-
         # We first find the tweet that the original tweet refers to.
         # Then we loop through `ref_tweets` to get the info for this tweet.
         ref_tweets = tweet['referenced_tweets']
@@ -153,34 +162,44 @@ def parse_tweet(tweet, includes_tweet_data, includes_user_data):
             tweet_id = ref_tweet['id']
 
             # Get tweet data from the refd tweet
-            for t in includes_tweet_data:
-                if t['id'] == tweet_id:
+            for included_tweet in includes_tweet_data:
+                if included_tweet['id'] == tweet_id:
+
+                    # Try to get refd tweet urls except pass
                     try:
-                        urls = t.get('entities', {}).get('urls', []) if 'entities' in t else []
+                        urls = included_tweet.get('entities', {}).get('urls', []) if 'entities' in included_tweet else []
                         expanded_urls = [url['expanded_url'] for url in urls if 'expanded_url' in url]
                         ref_tweet['urls'].extend(expanded_urls)
                         tweet['refd_urls'].extend(expanded_urls)
-                        ref_tweet['ref_author_id'] = t['author_id']
                     except:
-                        print(urls)
                         pass
 
-            # Get author data from the retweet
-            for t in includes_user_data:
-                if t['id'] == ref_tweet['ref_author_id']:
-                    ref_tweet['ref_author_username'] = t['username']
+                    # Try to get refd tweet author id except pass
+                    try:
+                        ref_tweet['ref_author_id'] = included_tweet['author_id']
+
+                        # Now if got the author id, we can get the author username from red tweets too
+                        for included_author in includes_user_data:
+                            if included_author['id'] == ref_tweet['ref_author_id']:
+                                ref_tweet['ref_author_username'] = included_author['username']
+                    except:
+                        pass
+
     tweet['all_urls'] = list(set(tweet['primary_urls'] + tweet['refd_urls']))
 
     return tweet
 
 
 def main(n_per_user, file_prefix, debug):
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d__%H--%M--%S')
+    file_prefix = f"{file_prefix}_{timestamp}_data.log"
+
     logging.basicConfig(filename=f"{file_prefix}_data.log", filemode='w', level=logging.INFO,
                         format='%(asctime)s: %(message)s', datefmt='%Y-%m-%d__%H--%M--%S')
     df = pd.read_csv("hydrated_users.csv", dtype={'id': str})
     if debug:
-        df = df.sample(n=10)
-    df = df.sample(n=10)
+        df = df.sample(n=7)
+    df = df.sample(n=7)
     user_ids = df['id'].tolist()
     tweet_controller(user_ids, n_per_user, file_prefix)
 
